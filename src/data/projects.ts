0e96201ac041890daaa1a11229783809004cee34
Maestro Haryor <ayomide.odewale1@gmail.com>
2023-07-14 00:36:58 +0100

    Project Placeholders

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ export const projects: Project[] = [
 		id: 1,
 		title: 'Braandly SAAS',
 		desc: 'A branding software for businesses and developers',
-		img: '/projects/yei-learn.png',
+		img: '/projects/braandly.webp',
 		link: 'https://braandly.com',
 		featured: true,
 		tags: ['ReactJS', 'NextJS', 'TailwindCSS', 'NodeJS', 'ExpressJS', 'MongoDB']
@@ -14,16 +14,35 @@ export const projects: Project[] = [
 		id: 2,
 		title: 'Jornally',
 		desc: 'A web publishing platform like Medium but cooler',
-		img: '/projects/buildfaster.png',
+		img: '/projects/jornally.webp',
 		link: 'https://jornally.com',
 		tags: ['ReactJS', 'NextJS', 'TailwindCSS', 'Laravel'],
+		featured: true
+	},
+
+	{
+		id: 5,
+		title: 'Lifetechfacts',
+		desc: 'A very cool blog I run and manage',
+		img: '/projects/lifetechfacts.webp',
+		link: 'https://thelifetechfacts.com',
+		tags: ['ReactJS', 'NextJS', 'Bootstrap', 'SASS', 'WordPress', 'NodeJS'],
+		featured: true
+	},
+	{
+		id: 8,
+		title: 'FoodMood',
+		desc: 'A food recipe app built with VueJS',
+		img: '/projects/foodmood.webp',
+		link: 'https://foodmood-final.vercel.app',
+		tags: ['VueJS', 'TailwindCSS'],
 		featured: true
 	},
 	{
 		id: 3,
 		title: 'Virem',
 		desc: 'A multivendor ecommerce platform and marketplace',
-		img: '/projects/react-emoji-search.png',
+		img: 'https://placehold.co/600x400?text=Virem+Multivendor+Platform',
 		link: 'https://virem.com.ng',
 		tags: ['ReactJS', 'NextJS', 'Bootstrap', 'SASS', 'WordPress', 'NodeJS']
 	},
@@ -31,24 +50,16 @@ export const projects: Project[] = [
 		id: 4,
 		title: 'Virem Vendors Platform',
 		desc: '',
-		img: '/projects/react-emoji-search.png',
+		img: 'https://placehold.co/600x400?text=Virem+Vendors+Platform',
 		link: 'https://thelifetechfacts.com',
 		tags: ['ReactJS', 'NextJS', 'Bootstrap', 'SASS', 'WordPress', 'NodeJS']
 	},
-	{
-		id: 5,
-		title: 'Lifetechfacts',
-		desc: 'A very cool blog I run and manage',
-		img: '/projects/react-emoji-search.png',
-		link: 'https://thelifetechfacts.com',
-		tags: ['ReactJS', 'NextJS', 'Bootstrap', 'SASS', 'WordPress', 'NodeJS'],
-		featured: true
-	},
+
 	{
 		id: 6,
 		title: 'LifetechFacts Training Platform',
 		desc: 'A multivendor ecommerce platform and marketplace',
-		img: '/projects/react-emoji-search.png',
+		img: 'https://placehold.co/600x400?text=Lifetechfacts+Training+Platform',
 		link: 'https://virem.com.ng',
 		tags: ['ReactJS', 'NextJS', 'TailwindCSS', 'NodeJS', 'ExpressJS']
 	},
@@ -56,24 +67,16 @@ export const projects: Project[] = [
 		id: 7,
 		title: 'Ayomideodewale.com',
 		desc: 'My current portfolio site',
-		img: '/projects/react-emoji-search.png',
+		img: 'https://placehold.co/600x400?text=Maestro+Website',
 		link: 'https://ayomideodewale.com',
 		tags: ['Svelte', 'SvelteKit', 'TailwindCSS', 'NodeJS', 'ExpressJS']
 	},
-	{
-		id: 8,
-		title: 'FoodMood',
-		desc: 'A food recipe app built with VueJS',
-		img: '/projects/react-emoji-search.png',
-		link: 'https://foodmood-final.vercel.app',
-		tags: ['VueJS', 'TailwindCSS'],
-		featured: true
-	},
+
 	{
 		id: 9,
 		title: 'Tailwind Blog',
 		desc: 'A SSR blog built with ExpressJS and rendered with the EJS templating system',
-		img: '/projects/react-emoji-search.png',
+		img: 'https://placehold.co/600x400?text=Tailwind+Blog',
 		link: 'https://virem.com.ng',
 		tags: ['NodeJS', 'ExpressJS', 'CSS', 'TailwindCSS', 'EJS', 'HTML']
 	},
@@ -81,7 +84,7 @@ export const projects: Project[] = [
 		id: 10,
 		title: 'Meals Recipe App',
 		desc: 'A meals recipe app built with React Native',
-		img: '/projects/react-emoji-search.png',
+		img: 'https://placehold.co/600x400?text=Meals+App',
 		github: 'https://github.com/Maestroharyor/meals-recipe-app',
 		tags: ['TypeScript', 'React Native']
 	},
@@ -89,7 +92,7 @@ export const projects: Project[] = [
 		id: 11,
 		title: 'Guess the number game App',
 		desc: 'A number game app built with React Native and TypeScript',
-		img: '/projects/react-emoji-search.png',
+		img: 'https://placehold.co/600x400?text=Numbers+Game',
 		github: 'https://github.com/Maestroharyor/guess-the-number-app',
 		tags: ['TypeScript', 'React Native']
 	},
@@ -97,7 +100,7 @@ export const projects: Project[] = [
 		id: 12,
 		title: 'Veegil Banking App',
 		desc: ' A banking app built with NextJS',
-		img: '/projects/react-emoji-search.png',
+		img: 'https://placehold.co/600x400?text=Veegil+App',
 		github: 'https://github.com/Maestroharyor/Veegil-Banking-App',
 		tags: ['ReactJS', 'NextJS', 'TailwindCSS', 'CSS']
 	},
@@ -105,7 +108,7 @@ export const projects: Project[] = [
 		id: 13,
 		title: 'Expense Tracker',
 		desc: 'A mobile expense tracker app built with flutter',
-		img: '/projects/react-emoji-search.png',
+		img: 'https://placehold.co/600x400?text=Expense+Tracker',
 		github: 'https://github.com/Maestroharyor/expense-tracker-flutter',
 		tags: ['Flutter']
 	},
@@ -114,7 +117,7 @@ export const projects: Project[] = [
 		id: 14,
 		title: 'Text Editor',
 		desc: 'A simple text editor app built with HTML, CSS, and JavaScript',
-		img: '/projects/react-emoji-search.png',
+		img: 'https://placehold.co/600x400?text=Text+Editor',
 		link: 'https://virem.com.ng',
 		tags: ['HTML', 'CSS', 'JavaScript']
 	}
