b03c9cf1a077427f764dbbab060576c801e4db7a
Ayomide Odewale <ayomide.odewale1@gmail.com>
2024-03-09 10:43:42 +0100

    Experince and resume update

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -3,8 +3,18 @@ import type { Experience } from '../types';
 export const experiences: Experience[] = [
 	{
 		year: '2023 - Present',
-		role: 'Software Developer',
-		company: 'HELPPO AFRICA',
+		role: 'Frontend Designer',
+		company: 'BPOSeats (Cebu, Philippines)',
+		isCurrent: true,
+		description:
+			'Spearheading User Interface design and writing high-quality and functional code for the tech. Achieved a 20% increase in user engagement by improving responsiveness and accessibility.',
+		stacks: ['Figma', 'JavaScript', 'VuejS', 'SASS'],
+		link: 'https://www.bposeats.com'
+	},
+	{
+		year: '2023',
+		role: 'Senior Software Developer',
+		company: 'Helppo Africa (Lagos, Nigeria)',
 		isCurrent: true,
 		description:
 			'Built a fintech application/platform for the organization and its users. Implemented secure payment processing and financial transaction management functionalities. Designed and developed user-friendly interfaces for seamless user experience',
@@ -14,7 +24,7 @@ export const experiences: Experience[] = [
 	{
 		year: '2022 - 2023',
 		role: 'Frontend Developer',
-		company: 'SUMMITECH COMPUTING LIMITED',
+		company: 'Summitech Computing Limited (Lagos, Nigeria)',
 		isCurrent: false,
 		description:
 			'Development and Management of Enterprise software products with JavaScript and TypeScript related stacks.',
@@ -23,24 +33,45 @@ export const experiences: Experience[] = [
 	},
 	{
 		year: '2022',
-		role: 'Frontend Developer',
-		company: 'VEENDHQ',
+		role: 'Software Developer',
+		company: 'VEENDHQ (Wyoming, USA)',
 		isCurrent: false,
 		description:
 			'Built fintech application/platforms for the organization and its users. Leveraged React and NextJS, a powerful fullstack JavaScript framework for creating web applications, to develop and maintain enterprise-grade fintech software products.',
 		stacks: ['JavaScript', 'TypeScript', 'ReactJS', 'NextJS', 'React Native'],
 		link: 'https://veendhq.com'
-	},
-	{
-		year: '2021 - 2022',
-		role: 'Senior Frontend Developer',
-		company: 'JORNALLY',
-		isCurrent: false,
-		description:
-			'Led a team of 3 in building the platform using React/NextJS, TailwindCSS and PHP(Laravel).',
-		stacks: ['React', 'NextJS', 'TailwindCSS', 'PHP', 'Laravel'],
-		link: 'https://jornally.com'
 	}
+	// {
+	// 	year: '2021',
+	// 	role: 'Lead Software Developer',
+	// 	company: 'Fovero Technologies (Lagos, Nigeria)',
+	// 	isCurrent: false,
+	// 	description:
+	// 		'Led a team of developers in the design and development of software solutions, resulting in a 20% reduction in project development time through streamlined processes and efficient collaboration.',
+	// 	stacks: [
+	// 		'React',
+	// 		'NextJS',
+	// 		'VueJS',
+	// 		'NuxtJS',
+	// 		'TailwindCSS',
+	// 		'NodeJS',
+	// 		'ExpressJS',
+	// 		'MongoDB',
+	// 		'SQL',
+	// 		'WordPress'
+	// 	],
+	// 	link: 'https://foverotechnologies.com'
+	// }
+	// {
+	// 	year: '2020',
+	// 	role: 'Programming Instructor/Web Developer',
+	// 	company: 'New Horizon (Lagos, Nigeria)',
+	// 	isCurrent: false,
+	// 	description:
+	// 		'Taught Core Programming language concepts. Built and deployed cutting-edge web applications for other companies.',
+	// 	stacks: ['HTML', 'CSS', 'Bootstrap', 'Python', 'WordPress'],
+	// 	link: 'https://www.newhorizonsnigeria.com.ng/'
+	// }
 	// {
 	// 	year: '2020 - 2021',
 	// 	role: 'Fullstack Web Engineer',
@@ -51,16 +82,7 @@ export const experiences: Experience[] = [
 	// 	stacks: ['React', 'NextJS'],
 	// 	link: 'https://virem.com.ng'
 	// },
-	// {
-	// 	year: '2019 - 2020',
-	// 	role: 'Programming Instructor/Web Developer',
-	// 	company: 'NEW HORIZONS NIGERIA',
-	// 	isCurrent: false,
-	// 	description:
-	// 		'Teaching Core Programming language concepts. Built and deployed cutting-edge web applications for other companies.',
-	// 	stacks: ['CSS', 'Bootstrap', 'ReactJS', 'WordPress'],
-	// 	link: 'https://www.newhorizonsnigeria.com.ng/'
-	// },
+
 	// {
 	// 	year: '2017 - 2019',
 	// 	role: 'Freelance Web Developer',
